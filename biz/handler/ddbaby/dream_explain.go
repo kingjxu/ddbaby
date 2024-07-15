f002ee38681b97b22a30717f92f852e11ca8b44c
xujin.xj <xujin.xj@bytedance.com>
2024-07-15 21:15:02 +0800

    f
    Change-Id: Ic9c05119d7c6df2907e1fb9542d5516b5431b1fa

--- biz/handler/ddbaby/dream_explain.go
+++ biz/handler/ddbaby/dream_explain.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"errors"
 	"github.com/kingjxu/ddbaby/biz/model/ddbaby"
+	"github.com/kingjxu/ddbaby/service"
 	"github.com/sirupsen/logrus"
 )
 
 type DreamExplainHandler struct {
-	req *ddbaby.DreamExplainReq
+	req     *ddbaby.DreamExplainReq
+	Explain string
 }
 
 func NewDreamExplainHandler(req *ddbaby.DreamExplainReq) *DreamExplainHandler {
@@ -25,20 +27,28 @@ func (h *DreamExplainHandler) check() error {
 }
 
 func (h *DreamExplainHandler) Handle(ctx context.Context) (*ddbaby.DreamExplainResp, error) {
-	logrus.WithFields(logrus.Fields{"req": h.req})
+	logrus.WithContext(ctx).Infof("[DreamExplainHandler] req:%v", h.req)
 	if err := h.check(); err != nil {
-		logrus.WithFields(logrus.Fields{"err": err.Error()})
+		logrus.WithContext(ctx).Errorf("[DreamExplainHandler] check err:%v", err)
 		return h.newResp(ctx, -1, "param err"), nil
 	}
-
+	content, err := service.GetDreamExplain(ctx, h.req.GetDream())
+	if err != nil {
+		logrus.WithContext(ctx).Errorf("[DreamExplainHandler] get dream explain err:%v", err)
+		return h.newResp(ctx, -1, "get dream explain err"), nil
+	}
+	h.Explain = content
 	return h.newResp(ctx, 0, ""), nil
 }
 
 func (h *DreamExplainHandler) newResp(ctx context.Context, code int32, msg string) *ddbaby.DreamExplainResp {
-	return &ddbaby.DreamExplainResp{
+	resp := &ddbaby.DreamExplainResp{
 		BaseResp: &ddbaby.BaseResp{
 			StatusMessage: msg,
 			StatusCode:    code,
 		},
 	}
+	resp.Explain = &h.Explain
+
+	return resp
 }
