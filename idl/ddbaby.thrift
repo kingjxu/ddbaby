0b521ed0605d6b1f9fce4fe4a281e0b2b6b2e15b
xujin.xj <xujin.xj@bytedance.com>
2024-07-19 17:36:54 +0800

    f
    Change-Id: Iccd73803b5f98749bb937c70f33b2111acb8f1c9

--- idl/ddbaby.thrift
+++ idl/ddbaby.thrift
@@ -22,7 +22,36 @@ struct DreamExplainResp {
     255: BaseResp BaseResp;
 }
 
+struct PickNameReq {
+    1: optional string FamilyName (api.query="family_name");
+    2: optional string Gender (api.query="gender");
+    3: optional string NameLen (api.query="name_len");
+    4: optional string Remark (api.query="remark");
+
+}
+struct PickNameResp {
+    1: optional string ReqID;
+    2: optional string Explain;
+
+    255: BaseResp BaseResp;
+}
+
+struct NameFortuneReq {
+    1: optional string Name (api.query="name");
+
+}
+struct NameFortuneResp {
+    1: optional string ReqID;
+    2: optional string Explain;
+
+    255: BaseResp BaseResp;
+}
+
+
+
 service DDBabyService {
     HelloResp HelloMethod(1: HelloReq req) (api.get="/hello");
     DreamExplainResp DreamExplain(1: DreamExplainReq req) (api.get="/lyxz/dream_explain");
+    PickNameResp PickName(1: PickNameReq req) (api.get="/lyxz/pick_name");
+    NameFortuneResp NameFortune(1: NameFortuneReq req) (api.get="/lyxz/name_fortune");
 }
