f9677275804ee5a2db9b76e3042b8a68fd565eb0
xujin.xj <xujin.xj@bytedance.com>
2024-09-11 10:15:02 +0800

    f
    Change-Id: I2b997acc308512f9ae8d08f01fd2bba02d87f073

--- service/health_questions.go
+++ service/health_questions.go
@@ -2,12 +2,9 @@ package service
 
 import (
 	"context"
-	"github.com/kingjxu/ddbaby/dal/mysql"
 	"github.com/kingjxu/ddbaby/dal/mysql/health_evaluate"
 )
 
 func GetHealthQuestions(ctx context.Context, questionType string) ([]*health_evaluate.HealthEvaluationQuestions, error) {
-	var questions []*health_evaluate.HealthEvaluationQuestions
-	err := mysql.GetDB(ctx).Where("question_type = ?", questionType).Find(&questions).Error
-	return questions, err
+	return health_evaluate.GetQuestionsByType(ctx, questionType)
 }
