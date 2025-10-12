9a16721b2cc20ad521b53e280e8f6c05698413d2
xujin.xj <xujin.xj@bytedance.com>
2025-10-12 23:09:11 +0800

    f
    Change-Id: Ibb40adf89de5d878f19e227c34259da31c4160f4

--- service/get_order_info.go
+++ service/get_order_info.go
@@ -6,9 +6,32 @@ import (
 	"github.com/kingjxu/ddbaby/dal/mysql/jk"
 	"github.com/kingjxu/ddbaby/wx_new"
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
 func GetOrderInfo(ctx context.Context, orderId string) (*jk.JkOrder, error) {
+	for i := 0; i < 2; i++ {
+		jkOrder, err := jk.GetOrderByOrderID(ctx, orderId)
+		if err != nil {
+			return nil, fmt.Errorf("GetOrderByOrderID err:%w", err)
+		}
+		if jkOrder.Status == 20 {
+			return jkOrder, nil
+		}
+		if wx_new.IsOrderPaySuccess(ctx, orderId) {
+			logrus.WithContext(ctx).Infof("order %v pay success", jkOrder.OrderID)
+			jkOrder.Status = 20
+		}
+		if i == 1 {
+			return jkOrder, nil
+		}
+
+		if jkOrder.Status == 10 {
+			time.Sleep(time.Second * 2)
+			continue
+		}
+		return jkOrder, nil
+	}
 	jkOrder, err := jk.GetOrderByOrderID(ctx, orderId)
 	if err != nil {
 		return nil, fmt.Errorf("GetOrderByOrderID err:%w", err)
