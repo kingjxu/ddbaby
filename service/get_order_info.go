d53dc7ca046e403b839aa715689e6c50b691924f
xujin.xj <xujin.xj@bytedance.com>
2025-10-13 19:20:52 +0800

    f
    Change-Id: Ibf7616e0943db73fd7870a14424a060ba834e3d2

--- service/get_order_info.go
+++ service/get_order_info.go
@@ -6,33 +6,19 @@ import (
 	"github.com/kingjxu/ddbaby/dal/mysql/jk"
 	"github.com/kingjxu/ddbaby/wx_new"
 	"github.com/sirupsen/logrus"
-	"time"
 )
 
 func GetOrderInfo(ctx context.Context, orderId string) (*jk.JkOrder, error) {
-	for i := 0; i < 2; i++ {
-		jkOrder, err := jk.GetOrderByOrderID(ctx, orderId)
-		if err != nil {
-			return nil, fmt.Errorf("GetOrderByOrderID err:%w", err)
-		}
-		if jkOrder.Status == 20 {
-			return jkOrder, nil
-		}
-		if wx_new.IsOrderPaySuccess(ctx, orderId) {
-			logrus.WithContext(ctx).Infof("order %v pay success,i:%v", jkOrder.OrderID, i)
-			jkOrder.Status = 20
-		}
-		if i == 1 {
-			return jkOrder, nil
-		}
-
-		if jkOrder.Status == 10 {
-			logrus.WithContext(ctx).Infof("order %v pay failed,i:%v", jkOrder.OrderID, i)
-			time.Sleep(time.Second * 2)
-			continue
-		}
+	jkOrder, err := jk.GetOrderByOrderID(ctx, orderId)
+	if err != nil {
+		return nil, fmt.Errorf("GetOrderByOrderID err:%w", err)
+	}
+	if jkOrder.Status == 20 {
 		return jkOrder, nil
 	}
-
-	return nil, nil
+	if wx_new.IsOrderPaySuccess(ctx, orderId) {
+		logrus.WithContext(ctx).Infof("order %v pay success,i:%v", jkOrder.OrderID, i)
+		jkOrder.Status = 20
+	}
+	return jkOrder, nil
 }
