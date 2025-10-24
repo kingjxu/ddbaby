56f61b4da2a273d0755df63108826a81fc1ed5a0
xujin.xj <xujin.xj@bytedance.com>
2025-10-24 23:22:52 +0800

    f
    Change-Id: I4133f64061cec04b88b865e1137a719f4080365e

--- biz/handler/jk_create_order.go
+++ biz/handler/jk_create_order.go
@@ -5,16 +5,13 @@ package handler
 import (
 	"context"
 	"errors"
-	"fmt"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	ddbaby "github.com/kingjxu/ddbaby/biz/model/ddbaby"
 	"github.com/kingjxu/ddbaby/model"
 	"github.com/kingjxu/ddbaby/service"
 	"github.com/kingjxu/ddbaby/util"
 	"github.com/sirupsen/logrus"
-	"net/url"
-
-	"github.com/cloudwego/hertz/pkg/app"
-	"github.com/cloudwego/hertz/pkg/protocol/consts"
-	ddbaby "github.com/kingjxu/ddbaby/biz/model/ddbaby"
 )
 
 // JkCreateOrder .
@@ -76,7 +73,8 @@ func (h *JkCreateOrderHandler) Handle(ctx context.Context) (*ddbaby.JkCreateOrde
 		logrus.WithContext(ctx).Errorf("[JkCreateOrderHandler] service.CreateOrder err:%v", err)
 		return h.newResp(ctx, -1, "wx prepay err"), nil
 	}
-	h.resp.PayURL = util.Ptr(h5Url + "&redirect_url=" + url.QueryEscape(fmt.Sprintf("http://ddbaby.site/qa-form/dist/index.html#/pages/pay/index?order_id=%v", orderID)))
+	//	h.resp.PayURL = util.Ptr(h5Url + "&redirect_url=" + url.QueryEscape(fmt.Sprintf("http://ddbaby.site/qa-form/dist/index.html#/pages/pay/index?order_id=%v", orderID)))
+	h.resp.ProfessorURL = util.Ptr(h5Url)
 	h.resp.OrderID = util.Ptr(orderID)
 	h.resp.ProfessorURL = util.Ptr("https://work.weixin.qq.com/ca/cawcde1469cf4f6a58")
 	return h.resp, nil
