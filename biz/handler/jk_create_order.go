f852dd0ecf275e16b0d1405e5bef5f9c306f0df4
xujin.xj <xujin.xj@bytedance.com>
2025-12-13 13:50:58 +0800

    f
    Change-Id: I4ad71925a5d5a0a7d775c5f717806af4fcae43c6

--- biz/handler/jk_create_order.go
+++ biz/handler/jk_create_order.go
@@ -81,7 +81,7 @@ func (h *JkCreateOrderHandler) Handle(ctx context.Context) (*ddbaby.JkCreateOrde
 		OrderID: orderID,
 		Version: h.req.GetAccVer(),
 		BdVid:   h.req.GetBdVid(),
-	}, constdef.CTypeSubmit)
+	}, /*constdef.CTypeSubmit*/ constdef.CTypeAddWechat)
 	h.resp.PayURL = util.Ptr(h5Url + "&redirect_url=" + url.QueryEscape(fmt.Sprintf("http://ddbaby.site/dist/#/test/checkorder?order_id=%v", orderID)))
 	h.resp.OrderID = util.Ptr(orderID)
 	h.resp.ProfessorURL = util.Ptr(constdef.ProfessorUrl)
