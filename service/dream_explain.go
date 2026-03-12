7c0b0f3a6774a5f3a9fb993eb17ac2e55d1c4841
xujin.xj <xujin.xj@bytedance.com>
2026-03-12 12:03:36 +0800

    f
    Change-Id: I85a3607759089a2467ceacaa8f5fb2e4bb136386

--- service/dream_explain.go
+++ service/dream_explain.go
@@ -103,7 +103,7 @@ func GetTexasPokerDecisionV2(ctx context.Context, images []string, imageType str
 			coze.BuildUserQuestionObjects(messageObject, nil),
 		},
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 100*time.Second)
 	defer cancel()
 	logrus.WithContext(ctx).Infof("[GetTexasPokerDecisionV2] messageObject:%v", util.ToJSON(messageObject))
 	resp, err := cozeCli.Chat.Stream(ctx, req)
