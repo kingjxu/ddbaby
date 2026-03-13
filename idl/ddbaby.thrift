73f310871244b6272175de04dbd08e1e98a62529
xujin.xj <xujin.xj@bytedance.com>
2026-03-13 16:29:45 +0800

    f
    Change-Id: I95a2151e00b85bed6bcd3b97091c029022e70fc3

--- idl/ddbaby.thrift
+++ idl/ddbaby.thrift
@@ -61,6 +61,7 @@ struct TaLuoPredictResp {
 struct TexasPokerDecisionReq {
     1: optional list<string> Images (api.query="images");
     2: optional i64 ImageTime (api.query="image_time");
+    3: optional string ImageType (api.query="image_type");
 }
 struct TexasPokerDecisionResp {
     1: optional string Action;
