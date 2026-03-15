1dae6380df6f013949a8a6f4810594d73f348881
xujin.xj <xujin.xj@bytedance.com>
2026-03-15 10:31:50 +0800

    f
    Change-Id: Icf19a260858276578c43659fcd924a6142d3db11

--- ark/chat_complete.go
+++ ark/chat_complete.go
@@ -50,8 +50,10 @@ func TexasPokerDecision(ctx context.Context, imageURL string) string {
 	}
 
 	resp, err := client.CreateResponses(ctx, &responses.ResponsesRequest{
-		Thinking: &responses.ResponsesThinking{Type: util.Ptr(responses.ThinkingType_disabled)},
-		Model:    "ep-20251028100133-mj9v2",
+		Temperature: util.Ptr(0.5),
+		TopP:        util.Ptr(1.0),
+		Thinking:    &responses.ResponsesThinking{Type: util.Ptr(responses.ThinkingType_disabled)},
+		Model:       "ep-20251028100133-mj9v2",
 		Input: &responses.ResponsesInput{
 			Union: &responses.ResponsesInput_ListValue{
 				ListValue: &responses.InputItemList{ListValue: []*responses.InputItem{
